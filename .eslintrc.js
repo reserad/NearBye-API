990d274efe3efff6e94153c0997fbc01907e1a07
Alec Reser <reser13@gmail.com>
2023-05-22 18:13:17 -0400

    Small adjustment to match auth service eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    sourceType: 'module',
+    tsconfigRootDir: __dirname,
     tsconfigRootDir: './',
   },
   plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
